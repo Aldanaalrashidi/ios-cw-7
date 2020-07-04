80343eb2f0b74ea91f4d77ab557620ac757468d8
61-الدانة سعد الرشيدي <aldanaalrashidi0@gmail.com>
2020-07-04 20:51:46 +0300

    Done with bonus

--- IOSClasswork7/IOSClasswork7/viewController/MovieDetailsViewController.swift
+++ IOSClasswork7/IOSClasswork7/viewController/MovieDetailsViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class MovieDetailsViewController: UIViewController {
     
-    
-    var movieData : moviesModel = moviesModel(name: "Joker", release: 2000, actors: [""], rating: 10.0, rated: "PG-13")
+    // the ! is to give it a value later & it couldn't be nil (without a value), i can use it from another vc
+    var movieData : moviesModel!
     
     @IBOutlet weak var movieImg: UIImageView!
     @IBOutlet weak var movieName: UINavigationItem!
@@ -49,14 +49,13 @@ class MovieDetailsViewController: UIViewController {
         
         self.movieReleaseDate.text = String(movieData.movieReleaseDate)
         
-        self.actorImg1.image = UIImage(named: movieData.actors[0])
-        self.actorName1.text = movieData.actors[0]
-        self.actorImg2.image = UIImage(named: movieData.actors[1])
-        self.actorName2.text = movieData.actors[1]
-
-        self.actorImg3.image = UIImage(named: movieData.actors[2])
-        self.actorName3.text = movieData.actors[2]
-
+                self.actorImg1.image = UIImage(named: movieData.actors[0])
+                self.actorName1.text = movieData.actors[0]
+                self.actorImg2.image = UIImage(named: movieData.actors[1])
+                self.actorName2.text = movieData.actors[1]
+                self.actorImg3.image = UIImage(named: movieData.actors[2])
+                self.actorName3.text = movieData.actors[2]
+        
         
     }
     
